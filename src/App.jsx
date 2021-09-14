aa60dd2a5b2fe4f4738cd02960f1438ac46910f3
Sandro Ferraz <sergiosandro86@gmail.com>
2021-09-14 15:32:04 -0300

    Adding Tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import Tasks from "./components/Tasks";
+import {v4 as uuidv4} from 'uuid'
 
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import "./App.css";
 
 const App = () => {
@@ -17,9 +19,20 @@ const App = () => {
     },
   ]);
 
+  const handleTaskAddition = (taskTitle) => {
+      const newTask = [ ...tasks, {
+        title: taskTitle,
+        id: uuidv4(),
+        completed: false,
+      },
+    ];
+    setTasks(newTask)
+  };
+
   return (
     <>
       <div className="container">
+        <AddTask handleTaskAddition={handleTaskAddition}/>
         <Tasks tasks={tasks} />
       </div>
     </>
